d126635c8ff5abdd83a8ecc57199be9f77daccdf
dabi <dabich626626@gmail.com>
2023-10-08 16:46:27 +0200

    ver1

---
+++ src/test/java/com/example/demo/Application.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class Application {
+}
---
+++ src/test/java/com/example/demo/FacultyService.java
@@ -0,0 +1,46 @@
+package ru.hogwarts.school.service;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+@Service
+public class FacultyService {
+
+    private final HashMap‹Long, Faculty› faculties = new HashMap‹›();
+    private long count = 0;
+
+    public Faculty addFaculty(Faculty faculty) {
+        faculty.setId(count++);
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty findFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        if (!faculties.containsKey(faculty.getId())) {
+            return null;
+        }
+        faculties.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty deleteFaculty(long id) {
+        return faculties.remove(id);
+    }
+
+    // Service
+    public Collection‹Faculty› findByColor(String color) {
+        ArrayList‹Faculty› result = new ArrayList‹›();
+        for (Faculty faculty : faculties.values()) {
+            if (Objects.equals(faculty.getColor(), color)) {
+                result.add(faculty);
+            }
+        }
+        return result;
+    }
+}
