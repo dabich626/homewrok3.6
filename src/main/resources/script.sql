b0add392ef7e321e46553eab32d8a612f28a3dd3
dabi <dabich626626@gmail.com>
2023-10-20 22:39:56 +0200

    ver3

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -7,11 +7,19 @@ from faculty;
 select name, color
 from faculty;
 
-select name
-from faculty;
+select student from faculty
+where age between (10,20);
 
-select age
-from student;
+select name, count(name) from faculty
+GROUP BY name;
 
-select student
-from faculty;
+select name, count(name) from faculty
+group by name
+HAVING like '%o%';
+
+select  from student
+where name = id > age;
+
+select  from faculty
+ORDER BY age;
+
