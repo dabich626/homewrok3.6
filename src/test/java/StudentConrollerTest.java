9fe9f69249f0e3de8335a418968b2fe0dfced81a
dabi <dabich626626@gmail.com>
2023-11-01 15:51:21 +0200

    ver6

--- src/test/java/StudentConrollerTest.java
+++ src/test/java/StudentConrollerTest.java
@@ -33,4 +33,89 @@ public class StudentConrollerTest {
 
     @InjectMocks
     StudentController studentController;
-}
+    @Test
+    public void saveStudentTest() throws Exception {
+        final Long id = 1L;
+        final String name = "Harry";
+
+        JS0NObject stidentObject = new JSONObject();
+        studentObject.put("name", name);
+
+        Student student = new Student();
+        student. setId(id);
+        student.setName(name) ;
+
+        mhen(studentRepository.save(any(Student.class))).thenReturn(student) ;
+        when(studentRepository.FindById(any(Long.class))).thenReturn(Optional.of(student));
+
+        mockHvc.perform(MockHvcRequestBuilders
+                .post("/student")
+                .content(studentObject.tostring())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATTON_JSON))
+                .andExpect(status().is0k())
+                .andExpect(jsonPath("$.id").value(id))
+                        .andExpect(jsonPath("$.name").value(name));
+
+        mockHvc.perform(HOGkHVEREquestBuiLderS
+                .get("/student/4"
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is0k())
+                .andExpect(jsonPath("$.id") .value(id))
+                .andExpect(jsonPath("$.nane").value(name));
+
+        stidentObject.put("id", id);
+        mockHvc.perform(MockHvcRequestBuilders
+                .put("/student")
+                .content(stidentObject.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().is0k())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value(name));}
+
+        @Test
+
+        public void findStudentTest() throws Exception {
+            final Long id = 2L;
+            final String name = "ron"ж
+
+
+
+                    JsONObject StUdeMtObHect = new JsONODject();
+            studentObject.put("name",name);
+
+            Student student = new Student();
+            student.setId(id);
+            student.setName(name);
+
+            when(studentRepository.save(any(Student.class))).thenReturn(student); ;
+            when(studentRepository.findById(any(Long.class))).thenReturn (Optional.of(student)) ;
+
+            mockHvc.perform(MockHvcRequestBuilders
+                    .delete("/student/2")
+                    .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is0k()) ;}
+
+
+
+            @Test
+            void testGetStudent() throws Exception {
+
+                Long id = 1L;
+                String name = "Harry";
+                int age = 182;
+
+                Student student = student(id, name, age);
+
+                when(studentRepository.save(any(Student.class))).thenReturn(student) ;
+                when(studentRepository.findById(any(Long.class))).thenReturn(Optional.of(student));
+
+                mockMvc.perform(MockMvcRequest Builders
+                        .get("/student/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().is0k())
+                        .andExpect(jsonPath("$.id").value(id))
+                        .andExpect(jsonPath(("$.id").value(name))
+                        .andExpect (jsonPath("$ age").value(age));
+}}
